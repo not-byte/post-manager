79dfe721cf3f991f4484f0ffd710b40f13dc6d22
akolt19d <a.kolt.19d@gmail.com>
2024-04-11 12:25:56 +0200

    UI(sidebar): implemented sidebar layout

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
     import { AppRail, LightSwitch } from "@skeletonlabs/skeleton";
     import Logo from "./Logo.svelte";
+    import { page } from "$app/stores";
+
+    const listData = [
+        {
+            text: "Napisz post",
+            route: "/create"
+        },
+        {
+            text: "Konfiguracja",
+            route: "/config"
+        }
+    ]
 </script>
 
-<AppRail background="bg-grey-lightest dark:bg-black px-4 py-8" width="w-60">
+<AppRail background="bg-grey-lightest dark:bg-black px-4 py-8 corner" width="w-60">
     <svelte:fragment slot="lead">
-        <div class="flex items-center h-4 p-4 w-full hover:brightness-75 transition-all">
-            <a href="https://www.notbyte.com" class="grid grid-cols-[1fr_auto] gap-2">
+        <div class="flex items-center h-4 p-4 w-full">
+            <a href="https://www.notbyte.com" class="grid grid-cols-[1fr_auto] gap-2 hover:invert-[25%] transition-all">
                 <Logo className="h-7" />
-                <span class="text-xl font-semibold flex items-end">notByte</span>
             </a>
+            <span class="text-xl font-semibold flex items-end">notByte Dev</span>
         </div>
     </svelte:fragment>
     <ul class="grid grid-rows-2 gap-2 p-4">
-        {#each ["Napisz post", "Konfiguracja"] as el}
-            <li class="p-2 hover:bg-colors pl-4">{ el }</li>     
+        {#each listData as el}
+            <a href={el.route} class="hover:bg-[rgba(0,0,0,0.1)] dark:hover:bg-[rgba(255,255,255,0.1)] transition-[background-color_background-image] p-2 px-4 {($page.route.id == el.route) ? 'bg-colors text-white corner-br-md corner-grey-lightest dark:corner-black' : ''}">
+                <li>{ el.text }</li>     
+            </a>
         {/each}
     </ul>
     <svelte:fragment slot="trail">
